e9d691f3b48e3cce30ff42ebb7b989f51a005e16
asdfghj12345795 <55428385+asdfghj12345795@users.noreply.github.com>
2019-11-19 15:32:41 +0900

    submit_04

--- main.c
+++ main.c
@@ -4,10 +4,7 @@
 
 void main(void)
 {
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	char str[30] = "happy C programming";
 	
-	strcpy(dst,src);
-	
-	printf("copied string : %s", dst);
+	printf("STRING LENGTH of %s is %d", str, strlen(str));
 }
