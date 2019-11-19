0637652494c5e6141ef55a5a03b86815a47a56f8
asdfghj12345795 <55428385+asdfghj12345795@users.noreply.github.com>
2019-11-19 15:30:40 +0900

    submit_03

--- main.c
+++ main.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
-int main()
+void main(void)
 {
-	int i=0;
-	char str[4];
-	str[0]= 'a';
-	str[1]= 'b';
-	str[2]= 'c';
-	str[3]= '\0';
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
 	
-	printf("%s\n", str);
+	strcpy(dst,src);
 	
-	while(str[i]!='\0')
-	{	
-		printf("%c", str[i]);
-		i++;
-	}
-	return 0;
-
+	printf("copied string : %s", dst);
 }
